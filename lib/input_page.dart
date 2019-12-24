dbb649de2bb1c33db15abbf5211121b43d7af26b
Your Name <errolcostabir98@gmail.com>
2019-12-24 14:56:41 +0530

    made the code reusable

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -15,48 +15,41 @@ class _InputPageState extends State<InputPage> {
       body: Column(
         children: <Widget>[
           Expanded(child: Row(children: <Widget>[
-            Expanded(child: Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
-        borderRadius: BorderRadius.circular(10.0)
-      ),
-      ),),
-            Expanded(child: Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
-        borderRadius: BorderRadius.circular(10.0)
-      ),
-      ),),
+            Expanded(child: Reusable(colour: Color(0xFF1D1E33)),),
+            Expanded(child: Reusable(colour: Color(0xFF1D1E33)),),
           ],)),
-          Expanded(child: Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
-        borderRadius: BorderRadius.circular(10.0)
-      ),
-      ),),
+          Expanded(child: Reusable(colour: Color(0xFF1D1E33)),),
 
           Expanded(child: Row(children: <Widget>[
-            Expanded(child: Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
-        borderRadius: BorderRadius.circular(10.0)
-      ),
-      ),),
-            Expanded(child: Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
-        borderRadius: BorderRadius.circular(10.0)
-      ),
-      ),),
+            Expanded(child: Reusable(colour: Color(0xFF1D1E33)),),
+            Expanded(child: Reusable(colour: Color(0xFF1D1E33)),),
           ],
           )),
         ],
       ) 
+      );
+  }
+}
+
+class Reusable extends StatelessWidget {
+  
+  Reusable({@required this.colour});
+  final Color colour;
+
+  //const Reusable({
+   // Key key,
+  //}) : super(key: key);
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0)
+      ),
       );
   }
 }
